ed7a9a3841692d7adc2d46a068fd009531b516f4
Gady Piazza <gady90@gmail.com>
2021-07-15 14:24:53 +0300

    trial 1

--- apps/api/src/api.controller.ts
+++ apps/api/src/api.controller.ts
@@ -1,9 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
+import { JwtService } from '../../../libs/jwt/src';
 import { ApiService } from './api.service';
 
 @Controller()
 export class ApiController {
-  constructor(private readonly apiService: ApiService) {}
+  constructor(
+    private readonly apiService: ApiService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   @Get()
   getHello(): string {
